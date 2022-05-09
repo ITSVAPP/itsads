f9af6922063a26768114ccf1f955661490fdcb1a
119003 <it.da.fukase@iftc.co.jp>
2022-05-09 16:02:26 +0900

    一覧表示機能追加

--- src/main/java/com/example/its/domain/issue/IssueEntity.java
+++ src/main/java/com/example/its/domain/issue/IssueEntity.java
@@ -1,5 +1,7 @@
 package com.example.its.domain.issue;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +11,32 @@ public class IssueEntity {
 	private long id;
 	private String summary;
 	private String description;
+	private Date createdday;
+	private Date deadline;
+	private Date completionday;
+	private String createuser;
+	private int status;
+
+	public String getCreateuserView() {
+
+		if (createuser == null) {
+			return "未設定";
+		}
+
+		return createuser + "さん";
+	}
+
+	public String getStatusView() {
+
+		switch (status) {
+
+		case 1:
+			return "着手";
+
+		case 2:
+			return "完了";
+
+		}
+		return "未着手";
+	}
 }
