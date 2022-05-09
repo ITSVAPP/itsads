919e836aa99845304a59584af7c8b7810d0ed2de
119003 <it.da.fukase@iftc.co.jp>
2022-05-09 10:27:06 +0900

    first commit

---
+++ src/main/java/com/example/its/domain/issue/IssueRepository.java
@@ -0,0 +1,21 @@
+package com.example.its.domain.issue;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface IssueRepository {
+
+	@Select("select * from issues")
+	List<IssueEntity> findAll();
+
+	@Insert("insert into issues (summary, description) values (#{summary}, #{description})")
+	void insert(@Param("summary") String summary, @Param("description") String description);
+
+	@Select("select * from issues where id = #{issueId}")
+	IssueEntity findById(long issueId);
+}
